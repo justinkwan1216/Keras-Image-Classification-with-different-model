365e903b4febdff6512bc45c1192cd21771b4be2
justinkwan1216 <justinkwan19971216@hotmail.com>
2020-05-20 17:12:14 +0800

    add inference

--- get_net.py
+++ get_net.py
@@ -1,85 +1,132 @@
 from keras.models import Model, Sequential
-from keras.layers import Input, Flatten, Dense, Dropout, GlobalMaxPooling2D
+from keras.layers import Input, Flatten, Dense, Dropout, GlobalMaxPooling2D, BatchNormalization, GlobalAveragePooling2D
 from keras.applications.vgg16 import VGG16
 from keras.applications.resnet50 import ResNet50
 from keras.applications import InceptionResNetV2
-from keras.optimizers import Adam, RMSprop
+from keras.applications import NASNetMobile
+from keras.applications import MobileNetV2
+from keras.optimizers import Adam, RMSprop, SGD
 import efficientnet.keras as efn
+def get_MobileNetV2(IMAGE_SIZE,FREEZE_LAYERS,NUM_CLASSES,DROPOUT_RATE=0.5):
+    net = MobileNetV2(weights='imagenet', input_shape=IMAGE_SIZE, include_top=False)
+    
+    x = net.output
+    x = Flatten()(x)
+
+    # 增加 DropOut layer
+    if DROPOUT_RATE > 0:
+        x = Dropout(DROPOUT_RATE)(x)
+
+    # 增加 Dense layer，以 softmax 產生個類別的機率值
+    output_layer = Dense(NUM_CLASSES, activation='softmax', name='softmax')(x)
+
+    # 設定凍結與要進行訓練的網路層
+    net_final = Model(inputs=net.input, outputs=output_layer)
+    for layer in net_final.layers[:FREEZE_LAYERS]:
+        layer.trainable = False
+        if isinstance(layer, BatchNormalization):
+            layer.trainable = True
+    for layer in net_final.layers[FREEZE_LAYERS:]:
+         layer.trainable = True
+
+    opt = Adam(lr=1e-5)
+    #opt = SGD(lr=0.001, momentum=0.9)
+    # 使用 Adam optimizer，以較低的 learning rate 進行 fine-tuning
+    net_final.compile(optimizer=opt,
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+    return net_final
+
 def get_resnet50(IMAGE_SIZE,FREEZE_LAYERS,NUM_CLASSES,DROPOUT_RATE=0.5):
     net = ResNet50(include_top=False, weights='imagenet', input_tensor=None,
                input_shape=IMAGE_SIZE)
     x = net.output
     x = Flatten()(x)
 
- 
+    # 增加 DropOut layer
     if DROPOUT_RATE > 0:
         x = Dropout(DROPOUT_RATE)(x)
 
-
+    # 增加 Dense layer，以 softmax 產生個類別的機率值
+    #x = Dense(128, activation='relu')(x)
     output_layer = Dense(NUM_CLASSES, activation='softmax', name='softmax')(x)
 
-
+    # 設定凍結與要進行訓練的網路層
     net_final = Model(inputs=net.input, outputs=output_layer)
     for layer in net_final.layers[:FREEZE_LAYERS]:
         layer.trainable = False
     for layer in net_final.layers[FREEZE_LAYERS:]:
          layer.trainable = True
-           
 
-    net_final.compile(optimizer=Adam(lr=1e-5),
+    opt = Adam(lr=1e-3)
+    #opt = SGD(lr=0.001, momentum=0.9)
+    
+    # 使用 Adam optimizer，以較低的 learning rate 進行 fine-tuning
+    net_final.compile(optimizer=opt,
                   loss='categorical_crossentropy', metrics=['accuracy'])
     return net_final
 
 def get_VGG16(IMAGE_SIZE,FREEZE_LAYERS,NUM_CLASSES,DROPOUT_RATE=0.5):
-
+    #new_input = Input(shape=IMAGE_SIZE)
     net = VGG16(weights='imagenet', input_shape=IMAGE_SIZE, include_top=False)
     
     x = net.output
     x = Flatten()(x)
 
-
+    # 增加 DropOut layer
     if DROPOUT_RATE > 0:
         x = Dropout(DROPOUT_RATE)(x)
 
-
+    # 增加 Dense layer，以 softmax 產生個類別的機率值
     x = Dense(256, activation='relu')(x)
     output_layer = Dense(NUM_CLASSES, activation='softmax', name='softmax')(x)
 
-
+    # 設定凍結與要進行訓練的網路層
     net_final = Model(inputs=net.input, outputs=output_layer)
     for layer in net_final.layers[:FREEZE_LAYERS]:
         layer.trainable = False
+        if isinstance(layer, BatchNormalization):
+            layer.trainable = True
     for layer in net_final.layers[FREEZE_LAYERS:]:
          layer.trainable = True
-           
 
-    net_final.compile(optimizer=Adam(lr=1e-5),
+    #opt = Adam(lr=1e-5)
+    opt = optimizers.RMSprop(lr=2e-5)
+    #opt = SGD(lr=0.001, momentum=0.9)
+    # 使用 Adam optimizer，以較低的 learning rate 進行 fine-tuning
+    net_final.compile(optimizer=opt,
                   loss='categorical_crossentropy', metrics=['accuracy'])
     return net_final
 
 def get_efficientnet(IMAGE_SIZE,FREEZE_LAYERS,NUM_CLASSES,DROPOUT_RATE=0.2):
     net = efn.EfficientNetB0(weights="imagenet", include_top=False, input_shape=IMAGE_SIZE)
     x = net.output
-    x = GlobalMaxPooling2D(name="gap")(x)
+    #x = Flatten()(x)
+    #x = GlobalAveragePooling2D()(x)
 
     # net_final.add(layers.Flatten(name="flatten"))
     if DROPOUT_RATE > 0:
         x = Dropout(DROPOUT_RATE)(x)
     # net_final.add(layers.Dense(256, activation='relu', name="fc1"))
 
-
+    # 增加 Dense layer，以 softmax 產生個類別的機率值
+    #x = Dense(128, activation='relu')(x)
     output_layer = Dense(NUM_CLASSES, activation='softmax', name='softmax')(x)
 
     
-
+    # 設定凍結與要進行訓練的網路層
     net_final = Model(inputs=net.input, outputs=output_layer)
     for layer in net_final.layers[:FREEZE_LAYERS]:
         layer.trainable = False
+        if isinstance(layer, BatchNormalization):
+            layer.trainable = True
     for layer in net_final.layers[FREEZE_LAYERS:]:
-         layer.trainable = True
+        layer.trainable = True
+    
+    #opt = Adam(lr=1e-3)
+    opt = Adam(lr=1e-5)
     
     net_final.compile(loss="categorical_crossentropy",
-                      optimizer=RMSprop(lr=2e-5),metrics=["acc"])
+                      optimizer=opt, metrics=['accuracy'])
     return net_final
 def get_InceptionResNetV2(IMAGE_SIZE,FREEZE_LAYERS,NUM_CLASSES,DROPOUT_RATE=0.2):
     net = InceptionResNetV2(weights='imagenet', input_tensor=None, input_shape=IMAGE_SIZE,include_top=False)
@@ -87,24 +134,61 @@ def get_InceptionResNetV2(IMAGE_SIZE,FREEZE_LAYERS,NUM_CLASSES,DROPOUT_RATE=0.2)
     x = Flatten()(x)
 
 
-
+    # 增加 DropOut layer
     if DROPOUT_RATE > 0:
         x = Dropout(DROPOUT_RATE)(x)
 
-
+    # 增加 Dense layer，以 softmax 產生個類別的機率值
     x = Dense(128, activation='relu')(x)
     output_layer = Dense(NUM_CLASSES, activation='softmax', name='softmax')(x)
 
-
+    # 設定凍結與要進行訓練的網路層
     net_final = Model(inputs=net.input, outputs=output_layer)
     for layer in net_final.layers[:FREEZE_LAYERS]:
         layer.trainable = False
     for layer in net_final.layers[FREEZE_LAYERS:]:
          layer.trainable = True
            
-
+    # 使用 Adam optimizer，以較低的 learning rate 進行 fine-tuning
     net_final.compile(optimizer=Adam(lr=1e-5),
                   loss='categorical_crossentropy', metrics=['accuracy'])
     return net_final
-
+def get_NASNetMobile(IMAGE_SIZE,FREEZE_LAYERS,NUM_CLASSES,DROPOUT_RATE=0.5):
+    input_tensor = Input(shape=IMAGE_SIZE)
+    net = NASNetMobile(include_top=False, weights='imagenet', input_tensor=input_tensor)
+    x = net.output
+    x = Flatten()(x)
     
+    x = BatchNormalization()(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    
+    x = BatchNormalization()(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    
+    x = BatchNormalization()(x)
+
+    # 增加 DropOut layer
+    if DROPOUT_RATE > 0:
+        x = Dropout(DROPOUT_RATE)(x)
+
+    # 增加 Dense layer，以 softmax 產生個類別的機率值
+    #x = Dense(128, activation='relu')(x)
+    output_layer = Dense(NUM_CLASSES, activation='softmax', name='softmax')(x)
+
+    # 設定凍結與要進行訓練的網路層
+    net_final = Model(inputs=net.input, outputs=output_layer)
+    for layer in net_final.layers[:FREEZE_LAYERS]:
+        layer.trainable = False
+    for layer in net_final.layers[FREEZE_LAYERS:]:
+        layer.trainable = True
+
+    opt = Adam(lr=1e-3)
+    #opt = SGD(lr=0.001, momentum=0.9)
+    
+    # 使用 Adam optimizer，以較低的 learning rate 進行 fine-tuning
+    net_final.compile(optimizer=opt,
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+    return net_final
+
--- image_generator.py
+++ image_generator.py
@@ -1,7 +1,8 @@
 from keras.preprocessing.image import ImageDataGenerator
-
+# 透過 data augmentation 產生訓練與驗證用的影像資料
 def get_generator(TRAIN_DATA_DIR,IMAGE_SIZE,BATCH_SIZE,r_r,w_s_r,h_s_r,s_r,z_r,c_s_r,h_f,v_f,FILL_MODE,VALIDATION_SPLIT):
-    train_datagen = ImageDataGenerator(rotation_range=r_r,
+    train_datagen = ImageDataGenerator(rescale=1.0/255.0,
+                                   rotation_range=r_r,
                                    width_shift_range=w_s_r,
                                    height_shift_range=h_s_r,
                                    shear_range=s_r,
@@ -17,16 +18,16 @@ def get_generator(TRAIN_DATA_DIR,IMAGE_SIZE,BATCH_SIZE,r_r,w_s_r,h_s_r,s_r,z_r,c
         target_size=IMAGE_SIZE,
         batch_size=BATCH_SIZE,
         class_mode='categorical',
-        subset='training')
+        subset='training') # set as training data
 
     validation_generator = train_datagen.flow_from_directory(
         TRAIN_DATA_DIR,
         target_size=IMAGE_SIZE,
         batch_size=BATCH_SIZE,
         class_mode='categorical',
-        subset='validation')
+        subset='validation') # set as training data
 
-
+    # 輸出各類別的索引值
     for cls, idx in train_generator.class_indices.items():
         print('Class #{} = {}'.format(idx, cls))
 
